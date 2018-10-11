e8313d119338fa5c721bc0e46ace228daf6505ba
Christina <60021521@ironshools.org>
2018-10-11 09:43:08 -0600

    Added Chapter 1

---
+++ Chapter 1/1_13.java
@@ -0,0 +1,16 @@
+/*
+Author:Christina Vinzant
+Date:10/01/2018
+
+Sloving 2*2 linear equations
+*/
+class Ex_1_13 {
+	public static void main(String[] args) {
+		System.out.println( 44.5 * 0.55 - 50.2 * 5.9);
+		System.out.println( 3.4 * 0.55 - 50.2 * 2.1);
+		System.out.println( 271.705 / 103.55000000000001);
+		System.out.println( 3.4 * 5.9 - 44.5 * 2.1);
+		System.out.println( 3.4 * 0.55 - 50.2 * 2.1);
+		System.out.println( 73.39 / 103.55000000000001);
+	}
+}
---
+++ Chapter 1/Ex_1_9.java
@@ -0,0 +1,13 @@
+/*
+Author:Christina Vinzant
+Date: 9/21/2018
+
+solving for area
+*/
+
+class Ex_1_9 {
+	public static void main(String[] args) {
+		System.out.println(4.5 * 7.9); 
+		System.out.println(4.5 + 4.5 + 7.9 + 7.9);
+	}
+}
